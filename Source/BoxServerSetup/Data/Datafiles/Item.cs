3fc85ba887bb38a610258ba0290b7757c8d60ce9
ALCHEMY-ALAMBIK <sebastien.chine@gmail.com>
2024-10-09 15:53:44 +0200

    Update Item.cs
    Missing the ;

--- Source/BoxServerSetup/Data/Datafiles/Item.cs
+++ Source/BoxServerSetup/Data/Datafiles/Item.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Specialized;
 using System.Reflection;
-using System.Xml.Serialization
+using System.Xml.Serialization;
 
 using Server;
 
